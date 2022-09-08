27deb4b7fd69118bf43308fde0a47dbe1241f898
tkylesh <tks2n@mtmail.mtsu.edu>
2022-09-08 14:24:23 -0500

    intial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeistI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
